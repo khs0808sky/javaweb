df0fd8b6f795d6efdbc7729378c988e8807f7af3
khs0808sky <117973849+khs0808sky@users.noreply.github.com>
2025-07-17 17:02:03 +0900

    0717_Servlet

---
+++ src/main/java/app/labs/servlet/basic/CalcServlet2.java
@@ -0,0 +1,59 @@
+package app.labs.servlet.basic;
+
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Optional;
+
+/**
+ * Servlet implementation class CalcServlet2
+ */
+@WebServlet("/servlet/CalcServlet2")
+public class CalcServlet2 extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public CalcServlet2() {
+        super();
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// 입력 : null 처리가 중요		
+		// ofNullable : null이 아니면 값을 전달, null이면 empty전달
+		int num1 = Optional.ofNullable(request.getParameter("num1"))
+					.map(Integer::parseInt)
+					.orElse(0);		
+		String op = Optional.ofNullable(request.getParameter("operator"))
+					.orElse("+");		
+		int num2 = Optional.ofNullable(request.getParameter("num2"))
+					.map(Integer::parseInt)
+					.orElse(0);
+		
+		// 연산(Biz)
+		CalcService calc = new CalcService(num1, num2, op);
+		int result = calc.getResult();
+		
+		// 출력
+		RequestDispatcher rd = request.getRequestDispatcher("calc2.jsp");
+		request.setAttribute("result", Integer.toString(result));
+		rd.forward(request, response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doGet(request, response);
+	}
+
+}
---
+++ src/main/webapp/calc1.jsp
@@ -0,0 +1,25 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Insert title here</title>
+</head>
+<body>
+	<h3>Servlet 계산기</h3>
+    <!-- el로 contextPath 확인 -->
+    <form name="form1" action="${pageContext.request.contextPath}/servlet/CalcServlet" method="post">
+        <input type="number" name="num1" size="5" required>
+        <select name="operator">
+            <option value="+" selected>+</option>
+            <option value="-">-</option>
+            <option value="*">*</option>
+            <option value="/">/</option>
+        </select>
+        <input type="number" name="num2" size="5" required>
+        <button type="submit">계산</button>
+        <button type="reset">초기화</button>
+    </form>
+</body>
+</html>
